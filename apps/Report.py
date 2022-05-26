e249b8bb45c8d94d199a207e349a4c97edb1674f
ThomasXIONG151215 <60814001+ThomasXIONG151215@users.noreply.github.com>
2022-05-26 14:33:39 +0800

    Add files via upload

---
+++ apps/Report.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+def app():
+    #OPTIONS = {
+    #    "Air": pd.read_csv("Air.csv"),
+    #    "Water": pd.read_csv("Water.csv"),
+    #    "Steam": pd.read_csv("Steam.csv"),
+    #    "R134a": pd.read_csv("R134a.csv")
+    #}
+    
+    #pd.read_csv(fluidn)
+
+    st.write('app 2')
